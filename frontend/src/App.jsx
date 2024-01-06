99f4e9d31aab30d01476c5a24dff1e5c71371729
Jared Drueco <drueco03@gmail.com>
2024-01-06 11:37:48 -0700

    init patient/practitioner routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
-export default function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import PatientHome from './views/patient/PatientHome';
+import PractitionerDashboard from './views/practitioner/PractitionerDashboard';
+
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/patient/:patientID" element={<PatientHome />} />
+        <Route path="/practitioner/dashboard" element={<PractitionerDashboard />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
