a319dc7d43b3b7773e5a0a86b1400b78540f7e35
Matthew Davies <70969486+mfdavies@users.noreply.github.com>
2024-01-07 10:26:19 -0700

    Done Profile Additions

--- frontend/src/views/practitioner/components/Navbar.jsx
+++ frontend/src/views/practitioner/components/Navbar.jsx
@@ -37,13 +37,10 @@ const Navbar = () => {
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
           >
             <li>
-              <a className="justify-between">
+              <a href="/practitioner/profile" className="justify-between">
                 Profile
                 <span className="badge">New</span>
               </a>
-            </li>
-            <li>
-              <a>Settings</a>
             </li>
             <li>
               <a>Logout</a>
