9982fa779ee254914c67658e85b045f732e618f9
Matthew Davies <70969486+mfdavies@users.noreply.github.com>
2024-01-06 22:22:19 -0700

    Update main.py

--- backend/server/src/main.py
+++ backend/server/src/main.py
@@ -11,7 +11,24 @@ from flask_cors import CORS
 load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.getenv("SECRET_KEY")
-cred = credentials.Certificate(os.getenv("service"))
+
+def create_service_dict():
+    variables_keys = {
+        "type": os.getenv("TYPE"),
+        "project_id": os.getenv("PROJECT_ID"),
+        "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+        "private_key": os.getenv("PRIVATE_KEY"),
+        "client_email": os.getenv("CLIENT_EMAIL"),
+        "client_id": os.getenv("CLIENT_ID"),
+        "auth_uri": os.getenv("AUTH_URI"),
+        "token_uri": os.getenv("TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+        "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+        "universe_domain": os.getenv("UNIVERSE_DOMAIN")
+    }
+    return variables_keys
+
+cred = credentials.Certificate(create_service_dict())
 firebase_admin.initialize_app(cred)
 
 from conversation.views import conversation_blueprint
