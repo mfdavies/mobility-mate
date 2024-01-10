c6cbd09427ef2a4e06493490522b724e78e328eb
Owen Cooke <ocooke@ualberta.ca>
2024-01-10 16:27:12 -0700

    fix exercise modal bug

--- frontend/src/views/practitioner/components/Exercises.jsx
+++ frontend/src/views/practitioner/components/Exercises.jsx
@@ -39,7 +39,9 @@ const Exercises = () => {
         <h1 className="text-2xl font-semibold">Exercises</h1>
         <button
           className="btn bg-dark-teal text-white"
-          onClick={() => document.getElementById("new_exercise_modal").showModal()} // Corrected the modal ID
+          onClick={() =>
+            document.getElementById("new_exercise_modal").showModal()
+          }
         >
           <Plus />
           New Exercise
@@ -49,17 +51,24 @@ const Exercises = () => {
         {exercises.length > 0 ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-4 p-5">
             {exercises.map((exercise) => (
-              <div key={exercise.id} className="bg-white rounded-lg shadow-md p-4 flex flex-col">
-                <img src={exercise.image} alt={exercise.title} className="w-80 h-32 sm:h-48 object-fit rounded-t-lg" />
+              <div
+                key={exercise.id}
+                className="bg-white rounded-lg shadow-md p-4 flex flex-col"
+              >
+                <img
+                  src={exercise.image}
+                  alt={exercise.title}
+                  className="w-80 h-32 sm:h-48 object-fit rounded-t-lg"
+                />
                 <h2 className="mt-2 font-bold text-lg">{exercise.title}</h2>
-                <p className="text-gray-600 text-sm mt-1">{exercise.description}</p>
+                <p className="text-gray-600 text-sm mt-1">
+                  {exercise.description}
+                </p>
               </div>
             ))}
           </div>
         ) : (
-          <div className="text-left p-3 mt-4">
-            No exercises in collection.
-          </div>
+          <div className="text-left p-3 mt-4">No exercises in collection.</div>
         )}
       </div>
       <ExerciseModal />
